57246a5bcd92515511f0004a73ecd24ec9069b71
Fabian <1803fabian@gmail.com>
2024-06-08 15:24:23 +0200

    changed naming

--- src/pages/edit/AnnotationListItem.tsx
+++ src/pages/edit/AnnotationListItem.tsx
@@ -2,11 +2,11 @@ import {Box, Button} from "@chakra-ui/react";
 
 function AnnotationListItem({ annotation, clicked }: { annotation: any, clicked: any }) {
     return (
-        <Box className="annotation-list-item" bg={"lightgreen"} w={"100%"}>
+        <Box className="annotation-list-item" border={"1px"} borderColor={"#00ffff"} w={"100%"}>
             <div className="annotation-list-item__name">{annotation.name}</div>
             <div className="annotation-list-item__start">{annotation.start}</div>
             <div className="annotation-list-item__end">{annotation.end}</div>
-            <Button onClick={clicked}>Edit</Button>
+            <Button onClick={clicked}>Details</Button>
         </Box>
     );
 }
