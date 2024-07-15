5390fa53773f1967de4643a444948aba6cd29344
Fabian <1803fabian@gmail.com>
2024-07-15 19:02:27 +0200

    retrieve paragraph data from backend

--- src/pages/edit/Edit.tsx
+++ src/pages/edit/Edit.tsx
@@ -3,6 +3,7 @@ import {Box, Grid, GridItem} from "@chakra-ui/react";
 import AnnotatorSidebar from "./AnnotatorSidebar.tsx";
 import {useEffect, useState} from "react";
 import AnnotationContext from "./AnnotationContext.ts";
+import {fetchWrapper} from "../../api/fetcher.ts";
 
 export type Annotation = {
     id: number,
@@ -18,6 +19,12 @@ export type AnnotationPart = {
     end: number,
 }
 
+export type Paragraph = {
+    number: string,
+    title: string,
+    book: string,
+}
+
 function Edit() {
 
     //TODO: embed everything (editMode, focusedAnnotation, etc.) in the URL or maybe not bc of tabbing
@@ -29,6 +36,9 @@ function Edit() {
     const [annotations, setAnnotations] = useState<Annotation[]>([])
     const [textList, setTextList] = useState<any[]>([])
     const [newAnnotation, setNewAnnotation] = useState<Annotation>({id: -1, name: "", color: "", parts: []})
+
+    const [paragraph, setParagraph] = useState<Paragraph>({number: "", title: "", book: ""})
+
 
     const [deleteMode, setDeleteMode] = useState<boolean>(false)
 
@@ -74,176 +84,33 @@ function Edit() {
     }
 
     useEffect(() => {
-        const exampleText = [
-            {
-                "id": 1,
-                "text": "Die Sache ist frei von Sachmängeln, wenn sie bei Gefahrübergang den subjektiven Anforderungen, den objektiven Anforderungen und den Montageanforderungen dieser Vorschrift entspricht."
-            },
-            [
-                {
-                    "id": 2,
-                    "text": "Die Sache entspricht den subjektiven Anforderungen, wenn sie "
-                },
-                {
-                    "list": [
-                        {
-                            "title": "1.",
-                            "content": {
-                                "id": 3,
-                                "text": "die vereinbarte Beschaffenheit hat,"
-                            }
-                        },
-                        {
-                            "title": "2.",
-                            "content": {
-                                "id": 4,
-                                "text": "sich für die nach dem Vertrag vorausgesetzte Verwendung eignet und"
-                            }
-                        },
-                        {
-                            "title": "3.",
-                            "content": {
-                                "id": 5,
-                                "text": "mit dem vereinbarten Zubehör und den vereinbarten Anleitungen, einschließlich Montage- und Installationsanleitungen, übergeben wird."
-                            }
+        fetchWrapper.get('api/paragraphs/3', null, false).then((data) => {
+            setParagraph({number: data.paragraph, title: data.titel, book: data.gesetz.name})
+            setAnnotations(data.annotations.map((annotation: any) => {
+                return {
+                    id: annotation.id,
+                    name: annotation.name,
+                    color: "#c0afd5",
+                    parts: annotation.annotationAbschnitte.map((part: any) => {
+                        return {
+                            id: part.id,
+                            textID: part.textAbschnitt.id,
+                            start: part.start,
+                            end: part.end
                         }
-                    ]
-                },
-                {
-                    "id": 6,
-                    "text": "Zu der Beschaffenheit nach Satz 1 Nummer 1 gehören Art, Menge, Qualität, Funktionalität, Kompatibilität, Interoperabilität und sonstige Merkmale der Sache, für die die Parteien Anforderungen vereinbart haben."
+                    })
                 }
-            ],
-            [
-                {
-                    "id": 7,
-                    "text": "Soweit nicht wirksam etwas anderes vereinbart wurde, entspricht die Sache den objektiven Anforderungen, wenn sie "
-                },
-                {
-                    "list": [
-                        {
-                            "title": "1.",
-                            "content": {
-                                "id": 8,
-                                "text": "sich für die gewöhnliche Verwendung eignet,"
-                            }
-                        },
-                        {
-                            "title": "2.",
-                            "content": [
-                                {
-                                    "id": 9,
-                                    "text": "eine Beschaffenheit aufweist, die bei Sachen derselben Art üblich ist und die der Käufer erwarten kann unter Berücksichtigung "
-                                },
-                                {
-                                    "list": [
-                                        {
-                                            "title": "a)",
-                                            "content": {
-                                                "id": 10,
-                                                "text": "der Art der Sache und"
-                                            }
-                                        },
-                                        {
-                                            "title": "b)",
-                                            "content": {
-                                                "id": 11,
-                                                "text": "der öffentlichen Äußerungen, die von dem Verkäufer oder einem anderen Glied der Vertragskette oder in deren Auftrag, insbesondere in der Werbung oder auf dem Etikett, abgegeben wurden,"
-                                            }
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                        {
-                            "title": "3.",
-                            "content": {
-                                "id": 12,
-                                "text": "der Beschaffenheit einer Probe oder eines Musters entspricht, die oder das der Verkäufer dem Käufer vor Vertragsschluss zur Verfügung gestellt hat, und"
-                            }
-                        },
-                        {
-                            "title": "4.",
-                            "content": {
-                                "id": 13,
-                                "text": "mit dem Zubehör einschließlich der Verpackung, der Montage- oder Installationsanleitung sowie anderen Anleitungen übergeben wird, deren Erhalt der Käufer erwarten kann."
-                            }
-                        }
-                    ]
-                },
-                {
-                    "id": 14,
-                    "text": "Zu der üblichen Beschaffenheit nach Satz 1 Nummer 2 gehören Menge, Qualität und sonstige Merkmale der Sache, einschließlich ihrer Haltbarkeit, Funktionalität, Kompatibilität und Sicherheit. Der Verkäufer ist durch die in Satz 1 Nummer 2 Buchstabe b genannten öffentlichen Äußerungen nicht gebunden, wenn er sie nicht kannte und auch nicht kennen konnte, wenn die Äußerung im Zeitpunkt des Vertragsschlusses in derselben oder in gleichwertiger Weise berichtigt war oder wenn die Äußerung die Kaufentscheidung nicht beeinflussen konnte."
-                }
-            ],
-            [
-                {
-                    "id": 15,
-                    "text": "Soweit eine Montage durchzuführen ist, entspricht die Sache den Montageanforderungen, wenn die Montage "
-                },
-                {
-                    "list": [
-                        {
-                            "title": "1.",
-                            "content": {
-                                "id": 16,
-                                "text": "sachgemäß durchgeführt worden ist oder"
-                            }
-                        },
-                        {
-                            "title": "2.",
-                            "content": {
-                                "id": 17,
-                                "text": "zwar unsachgemäß durchgeführt worden ist, dies jedoch weder auf einer unsachgemäßen Montage durch den Verkäufer noch auf einem Mangel in der vom Verkäufer übergebenen Anleitung beruht."
-                            }
-                        }
-                    ]
-                }
-            ],
-            {
-                "id": 18,
-                "text": "Einem Sachmangel steht es gleich, wenn der Verkäufer eine andere Sache als die vertraglich geschuldete Sache liefert."
-            }
-        ]
-        const exampleAnnotations = [
-            {
-                id: 1,
-                name: "Müller",
-                color: "#d1e797",
-                parts:
-                    [
-                        {id: 4561, textID: 1, start: 90, end: 170},
-                        {id: 2456, textID: 1, start: 30, end: 59},
-                        {id: 4563, textID: 7, start: 20, end: 50},
-                    ]
-            },
-            {
-                id: 2,
-                name: "Schäfer",
-                color: "#d794c7",
-                parts: [
-                    {id: 2843, textID: 13, start: 45, end: 120},
-                    {id: 4640, textID: 7, start: 10, end: 40},
-                ]
-            },
-            {
-                id: 3,
-                name: "Mayer",
-                color: "#c0afd5",
-                parts: [
-                    {id: 34, textID: 1, start: 50, end: 150},
-                    {id: 53, textID: 4, start: 30, end: 54},
-                    {id: 75, textID: 10, start: 1, end: 13},
-                ]
-            }
-        ]
-        //fetch annotations
-        setAnnotations(exampleAnnotations)
-        setTextList(exampleText)
+            }))
+            setTextList(data.textAbschnitte)
+        })
     }, []);
 
+    useEffect(() => {
+        document.title = `${paragraph.number} ${paragraph.book} - ${paragraph.title}`
+    }, [paragraph]);
+
     return (
-        <AnnotationContext.Provider value={{focusedAnnotation, editMode, annotations, clickedCallback, exitEditMode, selectionChangeCallback, deleteMode, toggleDeleteMode}}>
+        <AnnotationContext.Provider value={{focusedAnnotation, editMode, annotations, clickedCallback, exitEditMode, selectionChangeCallback, deleteMode, toggleDeleteMode, paragraph}}>
             <Box m='50px'>
                 <Grid
                     templateColumns="12fr 3fr"
